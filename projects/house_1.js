b595102056ed915dfc4a98e950c217ae28046603
Ren Provey <ren.provey@gmail.com>
2010-09-29 16:53:11 -0400

    Updated content for HTML5 using the excellent HTML5 boilerplate examples.

---
+++ projects/house_1.js
@@ -0,0 +1,81 @@
+new Room({label: "Porch", size: 30});
+
+new Room({
+  label: "Front Room",
+  size: 30
+});
+
+new Room({
+  label: "Kitchen",
+  size: 40
+});
+
+new Room({
+  label: "Eating Space",
+  size: 30
+});
+
+new Room({
+  label: "Living room",
+  size: 50
+});
+
+new Room({
+  label: "Bathroom",
+  size: 10
+});
+
+new Room({
+  label: "Bedroom",
+  size: 30
+});
+
+new Room({
+  label: "Bedroom",
+  size: 30
+});
+
+new Room({
+  label: "Bathroom",
+  size: 10
+});
+
+new Room({
+  label: "Master Bedroom",
+  size: 50
+});
+
+new Room({
+  label: "Master Bathroom",
+  size: 20
+});
+
+new Room({
+  label: "Grill & Patio",
+  size: 30
+});
+
+new Room({
+  label: "Laundry",
+  size: 30
+});
+
+new Room({
+  label: "Shed & Gardening",
+  size: 30
+});
+
+new Room({
+  label: "Storage",
+  size: 60
+});
+
+new Room({
+  label: "Office",
+  size: 40
+});
+
+new Room({
+  label: "Workspace",
+  size: 60
+});
